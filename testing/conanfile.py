7b0ce8a74b118d9aac6366c295b44e8553d2e654
wahlm <wahlm@rocketmail.com>
2018-03-14 12:12:16 +0100

    Tabs to spaces

--- testing/conanfile.py
+++ testing/conanfile.py
@@ -2,9 +2,9 @@ from conans import ConanFile
 import os
 
 class TestConan(ConanFile):
-	settings = "os", "compiler", "build_type", "arch"
-	requires = "libA/0.1.latest@mawa/test", \
-				"libB/0.1.latest@mawa/test", \
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "libA/0.1.latest@mawa/test", \
+                "libB/0.1.latest@mawa/test", \
                 "libD/0.1.latest@mawa/test"
 				
-	generators = "visual_studio"
+    generators = "visual_studio"
